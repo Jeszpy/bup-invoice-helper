88a5b51c260091d63fe77270f1a8c75fcec6cd03
Jeszpy <hleb.lukashonak@gmail.com>
2023-08-03 05:58:41 +0300

    ref: add logs to watch file structure on render.com

--- apps/server/src/modules/bootstrap/bootstrap.service.ts
+++ apps/server/src/modules/bootstrap/bootstrap.service.ts
@@ -60,6 +60,10 @@ export class BootstrapService implements OnApplicationBootstrap {
 
     async onApplicationBootstrap() {
         try {
+            console.log(process.cwd())
+            console.log(path.join(process.cwd(), '..'))
+            console.log(path.join(process.cwd(), '..', '..'))
+            console.log(path.join(process.cwd(), '..', '..', '..',))
             await this.createFolders([this.pathToExcelFolder, this.pathToPdfFolder])
             await this.unzipXlsxTemplate()
         } catch (e) {
